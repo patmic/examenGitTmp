d412b12c2e8651ad9623a71629f91ac65c1394b9
pat_mic <pat_mic@hotmail.com>
2022-07-01 10:44:25 -0500

    cambios en el main

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola todos!");
         System.out.println("bye - bye..!");
+        
     }
 }
